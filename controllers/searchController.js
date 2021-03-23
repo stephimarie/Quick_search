4bfa7ffdb491c6d4f431b49605a253c895bdc0a3
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 15:53:09 -0700

    try/catch search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -10,7 +10,12 @@ module.exports = {
         const searching = await axios.get(
           `https://www.googleapis.com/books/v1/volumes?q=${searchParam}&key=${KEY}&maxResults=20`
         );
-      } 
+        res.json(searching);
+        console.log("searching ", searching);
+      } catch (err) {
+          console.log("oh no, we have a get search err", err);
+          res.send("couldn't search books", err);
+      }
     },
   };
   
