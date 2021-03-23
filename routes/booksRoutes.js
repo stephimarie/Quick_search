10ae601acd1450eebbb33dc75033f400151c4af2
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 16:21:37 -0700

    set up book routes

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
+const { getSaved, saveBook } = require("../controllers/dbController");
+
+router.get("/", getSaved);
+router.post("/", saveBook);
+
+module.exports = router;
 
