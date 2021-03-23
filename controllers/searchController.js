17aa31bd8c0eda5368b8f116e477375dce5c71c5
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 15:48:17 -0700

    axios link

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,8 +3,14 @@ const axios = require("axios");
 
 module.exports = {
     searchBooks: async (req, res) => {
-        console.log("searched books");
-        const searchParam = req.body.searchInput;
-        const Key = process.env.API_KEY;
-    }
-};
+      console.log("searched books");
+      const searchParam = req.body.searchInput;
+      const KEY = process.env.API_KEY;
+      try {
+        const searching = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${searchParam}&key=${KEY}&maxResults=20`
+        );
+      } 
+    },
+  };
+  
