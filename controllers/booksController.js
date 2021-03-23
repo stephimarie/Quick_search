df360bd389cf5f1ecda8c640377bf79235067f13
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 15:57:31 -0700

    get saved books from db

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,1 +1,17 @@
 const Book = require("../models/bookModels");
+
+module.exports = {
+    
+    getSaved: async (req, res) => {
+        console.log("got the books");
+
+        try {
+          const allBooks = await Book.find();
+          res.json(allBooks);
+        } catch (err) {
+          console.log("oh no, get books err", err);
+          res.send("couldn't get books", err);
+        }
+    
+    },
+};
