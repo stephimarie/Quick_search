81b6fc902492dae5d9ca3401ff9f5ae46dd960ca
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 15:45:28 -0700

    search book param

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,2 +1,10 @@
 const Book = require("../models/bookModels");
 const axios = require("axios");
+
+module.exports = {
+    searchBooks: async (req, res) => {
+        console.log("searched books");
+        const searchParam = req.body.searchInput;
+        const Key = process.env.API_KEY;
+    }
+};
