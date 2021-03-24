ad82b5ec728aedfdb10e0006ba23e0fae80fd691
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 17:13:24 -0700

    save button

--- client /src/Components/savebutton.js
+++ client /src/Components/savebutton.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./saveButton.css";
 
-// The ...props means, spread all of the passed props onto this element
-// That way we don't have to define them all individually
 function SaveButton(props) {
   return (
     <span
