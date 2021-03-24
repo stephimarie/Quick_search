557a7b86c2f8d1fcfb386b300fc26d4f36345337
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 17:16:11 -0700

    footer

---
+++ client /src/Components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="card-footer ">
+      <p>© 2021 Lauren Hutchinson</p>
+      <p>Google Book Search</p>
+    </div>
+  );
+};
+
+export default Footer;
