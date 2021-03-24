90a66e137ccca87b0890f4c3d8168e203155c07d
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-23 17:20:53 -0700

    Nav Bar

---
+++ client /src/Components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <p href="#" className="navbar-brand" style={{ paddingTop: "20px" }}>
+          Google Books Quick Search
+        </p>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link to="/">Search</Link>
+            <Link to="/saved">Saved</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
